96d53bb6aaad1bc9e439fc9f5103b5f7caf4ae1c
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-04-06 14:09:37 +0000

    fix tailwindcss

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <>
-      <h1>Hi</h1>
+      <h1 className="text-9xl">Text</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 };
