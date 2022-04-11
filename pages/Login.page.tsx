29c82f15d73f361675bf3abdf8a47d713cbb1db1
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-04-11 10:03:53 +0000

    protect routes

--- pages/Login.page.tsx
+++ pages/Login.page.tsx
@@ -1,9 +1,16 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import { isatty } from "tty";
 import { useAuthContext } from "../utils/Context/AuthContext";
 
 const Login: NextPage = () => {
+  const { isAuth } = useAuthContext();
+  const router = useRouter();
+
+  if (isAuth) {
+    router.push("/");
+    return <></>;
+  }
+
   return (
     <>
       <h1>Login</h1>
--- pages/SignUp.page.tsx
+++ pages/SignUp.page.tsx
@@ -1,4 +1,15 @@
+import { useRouter } from "next/router";
+import { useAuthContext } from "../utils/Context/AuthContext";
+
 const SignUp = () => {
+  const { isAuth } = useAuthContext();
+  const router = useRouter();
+
+  if (isAuth) {
+    router.push("/");
+    return <></>;
+  }
+
   return (
     <>
       <h1>SignUp</h1>
