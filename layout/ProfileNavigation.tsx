6a65cf237d6a9937fb9c869b530bb0ce6f53b429
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-04-20 09:25:19 +0000

    add active in tabs

--- layout/ProfileNavigation.tsx
+++ layout/ProfileNavigation.tsx
@@ -1,16 +1,26 @@
 import { CreditCardIcon, IdentificationIcon } from "@heroicons/react/outline";
 import Link from "next/link";
 import { FC, ReactElement, ReactNode } from "react";
+import { useRouter } from "next/router";
 
 const ProfileNavigationLayout = (page: ReactElement) => {
+  const router = useRouter();
+  const currentPath = router.pathname.toLowerCase();
+
   return (
     <>
       <div className="fixed w-60">
-        <TabLink Icon={<IdentificationIcon />} text="Account" link="/Profile" />
+        <TabLink
+          Icon={<IdentificationIcon />}
+          text="Account"
+          link="/Profile"
+          active={currentPath === "/profile"}
+        />
         <TabLink
           Icon={<CreditCardIcon />}
           text="Payment"
           link="/Profile/Payment"
+          active={currentPath === "/profile/payment"}
         />
       </div>
       <div className="mb-20 ml-60 w-full max-w-4xl px-12">{page}</div>
@@ -18,13 +28,20 @@ const ProfileNavigationLayout = (page: ReactElement) => {
   );
 };
 
-const TabLink: FC<{ Icon: ReactNode; text: string; link: string }> = ({
-  Icon,
-  text,
-  link,
-}) => (
+const TabLink: FC<{
+  Icon: ReactNode;
+  text: string;
+  link: string;
+  active: boolean;
+}> = ({ Icon, text, link, active }) => (
   <Link href={link}>
-    <a className="flex w-full items-center rounded-md p-3 text-sm font-medium dark:text-neutral-300 hover:dark:bg-neutral-800  hover:dark:text-neutral-200">
+    <a
+      className={`${
+        active
+          ? "dark:text-indigo-300"
+          : "dark:text-neutral-300 hover:dark:text-neutral-200"
+      } flex w-full items-center rounded-md p-3 text-sm font-medium  hover:dark:bg-neutral-800  `}
+    >
       <span className="mr-2 h-6 w-6">{Icon}</span>
       {text}
     </a>
