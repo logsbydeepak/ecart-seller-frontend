533896ecd39fe6abbdf7313fddc79b5630ccee0d
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-05-05 08:08:52 +0000

    add onError and onSuccess

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -1,37 +1,42 @@
 import { object } from "yup";
 import Link from "next/link";
 import { NextPage } from "next";
+import { useImmer } from "use-immer";
 import { useRouter } from "next/router";
 import { useMutation } from "react-query";
 import { MailIcon } from "@heroicons/react/solid";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { SubmitHandler, useForm, UseFormGetValues } from "react-hook-form";
 
+import Spinner from "~/components/Spinner";
 import { gqlRequest } from "~/utils/helper/gql";
 import LoginQuery from "~/utils/gql/User/Login.gql";
 import { email, password } from "~/utils/validation";
 import { useAuthContext } from "~/context/AuthContext";
 import InputWithLeftIcon from "~/components/Input/InputWithLeftIcon";
 import PasswordInputWithLeftIcon from "~/components/Input/PasswordInputWithLeftIcon";
-import Spinner from "~/components/Spinner";
-import { useState } from "react";
 
-interface LoginInputType {
+interface LoginFormType {
   email: string;
   password: string;
 }
 
 const schema = object({ email, password });
 
-const loginRequest = (getValues: UseFormGetValues<LoginInputType>) =>
+const loginRequest = (getValues: UseFormGetValues<LoginFormType>) =>
   gqlRequest(LoginQuery, getValues());
 
 const Login: NextPage = () => {
   const { isAuth, setIsAuth } = useAuthContext();
   const router = useRouter();
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [isSuccess, setIsSuccess] = useState(false);
+  const [requestStatus, setRequestStatus] = useImmer({
+    isLoading: false,
+    isSuccess: false,
+    isError: false,
+  });
+
+  const { isLoading, isSuccess, isError } = requestStatus;
 
   const {
     register,
@@ -39,35 +44,50 @@ const Login: NextPage = () => {
     setError,
     getValues,
     formState: { errors },
-  } = useForm<LoginInputType>({ resolver: yupResolver(schema) });
+  } = useForm<LoginFormType>({ resolver: yupResolver(schema) });
 
-  const { mutateAsync, isError } = useMutation(loginRequest, {
-    retry: 3,
-  });
+  const onError = () =>
+    setRequestStatus((draft) => {
+      draft.isError = true;
+      draft.isLoading = false;
+    });
 
-  const onSubmit: SubmitHandler<LoginInputType> = async () => {
-    setIsLoading(true);
-    const createSession = await mutateAsync(getValues);
-    const typename = createSession.createSession.__typename;
+  const onSuccess = (data: any) => {
+    const createUser = data.createUser;
+    const typename = createUser.__typename;
 
-    if (typename === "ErrorResponse") {
-      setIsLoading(false);
-      setError(
-        "email",
-        { message: "invalid email or password" },
-        { shouldFocus: true }
-      );
-      setError(
-        "password",
-        { message: "invalid email or password" },
-        { shouldFocus: true }
-      );
+    if (
+      createUser.title === "AUTHENTICATION" &&
+      createUser.message === "email already exist"
+    ) {
+      setRequestStatus((draft) => {
+        draft.isLoading = false;
+      });
+      setError("email", { message: createUser.message }, { shouldFocus: true });
     }
 
     if (typename === "User") {
-      setIsSuccess(true);
+      setRequestStatus((draft) => {
+        draft.isSuccess = true;
+      });
       setIsAuth(true);
-      router.push("/App");
+    }
+  };
+
+  const { mutateAsync } = useMutation(loginRequest, {
+    retry: 3,
+    onError,
+    onSuccess,
+  });
+
+  const onSubmit: SubmitHandler<LoginFormType> = async () => {
+    try {
+      setRequestStatus((draft) => {
+        draft.isLoading = true;
+      });
+      await mutateAsync(getValues);
+    } catch (error) {
+      onError();
     }
   };
 
@@ -77,63 +97,59 @@ const Login: NextPage = () => {
   }
 
   return (
-    <>
-      <div className="flex flex-col items-center	justify-center py-20">
-        <div className="rounded-lg border-2 px-10 py-16 ">
-          <h1 className="pb-4 text-center text-4xl font-bold text-slate-900 ">
-            Welcome back
-          </h1>
-          <p className="mb-8 text-center">
-            Don&apos;t have an account?
-            <Link href="/SignUp">
-              <a className="ml-2 text-indigo-600 hover:text-indigo-500 hover:underline">
-                SignUp
-              </a>
-            </Link>
+    <div className="flex flex-col items-center	justify-center py-20">
+      <div className="rounded-lg border-2 px-10 py-16 ">
+        <h1 className="pb-4 text-center text-4xl font-bold text-slate-900 ">
+          Welcome back
+        </h1>
+        <p className="mb-8 text-center">
+          Don&apos;t have an account?
+          <Link href="/SignUp">
+            <a className="ml-2 text-indigo-600 hover:text-indigo-500 hover:underline">
+              SignUp
+            </a>
+          </Link>
+        </p>
+
+        {isError && (
+          <p className="pb-4 text-center text-red-500">Something went wrong</p>
+        )}
+
+        {isSuccess && (
+          <p className="pb-4 text-center text-green-500">
+            User Created Successfully
           </p>
+        )}
 
-          {isError && (
-            <p className="pb-4 text-center text-red-500">
-              Something went wrong
-            </p>
-          )}
+        <form className="w-96" onSubmit={handleSubmit(onSubmit)}>
+          <fieldset disabled={isLoading}>
+            <InputWithLeftIcon
+              register={register("email")}
+              label="Email"
+              errorMessage={errors.email?.message}
+              placeholder="example@abc.com"
+              Icon={<MailIcon />}
+            />
 
-          {isSuccess && (
-            <p className="pb-4 text-center text-green-500">
-              User Created Successfully
-            </p>
-          )}
+            <PasswordInputWithLeftIcon
+              register={register("password")}
+              className="mt-4"
+              label="Password"
+              type="password"
+              errorMessage={errors.password?.message}
+              placeholder="strong password"
+            />
 
-          <form className="w-96" onSubmit={handleSubmit(onSubmit)}>
-            <fieldset disabled={isLoading}>
-              <InputWithLeftIcon
-                register={register("email")}
-                label="Email"
-                errorMessage={errors.email?.message}
-                placeholder="example@abc.com"
-                Icon={<MailIcon />}
-              />
-
-              <PasswordInputWithLeftIcon
-                register={register("password")}
-                className="mt-4"
-                label="Password"
-                type="password"
-                errorMessage={errors.password?.message}
-                placeholder="strong password"
-              />
-
-              <button
-                type="submit"
-                className="mt-8 flex h-12 w-full justify-center rounded-md bg-indigo-600 py-3 text-white hover:bg-indigo-500 disabled:bg-neutral-900"
-              >
-                {isLoading ? <Spinner className="h-5 w-5" /> : "SignUp"}
-              </button>
-            </fieldset>
-          </form>
-        </div>
+            <button
+              type="submit"
+              className="mt-8 flex h-12 w-full justify-center rounded-md bg-indigo-600 py-3 text-white hover:bg-indigo-500 disabled:bg-neutral-900"
+            >
+              {isLoading ? <Spinner className="h-5 w-5" /> : "SignUp"}
+            </button>
+          </fieldset>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/pages/SignUp.page.tsx
+++ src/pages/SignUp.page.tsx
@@ -1,20 +1,20 @@
 import { object } from "yup";
 import Link from "next/link";
 import { NextPage } from "next";
+import { useImmer } from "use-immer";
 import { useRouter } from "next/router";
 import { useMutation } from "react-query";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { EmojiHappyIcon, MailIcon } from "@heroicons/react/solid";
 import { SubmitHandler, useForm, UseFormGetValues } from "react-hook-form";
 
+import Spinner from "~/components/Spinner";
 import { gqlRequest } from "~/utils/helper/gql";
 import SignUpQuery from "~/utils/gql/User/SignUp.gql";
 import { useAuthContext } from "~/context/AuthContext";
 import { name, email, password } from "~/utils/validation";
 import InputWithLeftIcon from "~/components/Input/InputWithLeftIcon";
 import PasswordInputWithLeftIcon from "~/components/Input/PasswordInputWithLeftIcon";
-import Spinner from "~/components/Spinner";
-import { useState } from "react";
 
 const schema = object({ name, email, password });
 const signUpRequest = (getValues: UseFormGetValues<SignUpFormType>) =>
@@ -30,8 +30,13 @@ const SignUp: NextPage = () => {
   const { isAuth, setIsAuth } = useAuthContext();
   const router = useRouter();
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [isSuccess, setIsSuccess] = useState(false);
+  const [requestStatus, setRequestStatus] = useImmer({
+    isLoading: false,
+    isSuccess: false,
+    isError: false,
+  });
+
+  const { isLoading, isSuccess, isError } = requestStatus;
 
   const {
     register,
@@ -41,28 +46,48 @@ const SignUp: NextPage = () => {
     formState: { errors },
   } = useForm<SignUpFormType>({ resolver: yupResolver(schema) });
 
-  const { mutateAsync, isError } = useMutation(signUpRequest, {
-    retry: 3,
-  });
+  const onError = () =>
+    setRequestStatus((draft) => {
+      draft.isError = true;
+      draft.isLoading = false;
+    });
 
-  const onSubmit: SubmitHandler<SignUpFormType> = async () => {
-    setIsLoading(true);
-    const signUpUser = await mutateAsync(getValues);
-    const createUser = signUpUser.createUser;
+  const onSuccess = (data: any) => {
+    const createUser = data.createUser;
     const typename = createUser.__typename;
 
     if (
       createUser.title === "AUTHENTICATION" &&
       createUser.message === "email already exist"
     ) {
-      setIsLoading(false);
+      setRequestStatus((draft) => {
+        draft.isLoading = false;
+      });
       setError("email", { message: createUser.message }, { shouldFocus: true });
     }
 
     if (typename === "User") {
-      setIsSuccess(true);
+      setRequestStatus((draft) => {
+        draft.isSuccess = true;
+      });
       setIsAuth(true);
-      router.push("/App");
+    }
+  };
+
+  const { mutateAsync } = useMutation(signUpRequest, {
+    retry: 3,
+    onError,
+    onSuccess,
+  });
+
+  const onSubmit: SubmitHandler<SignUpFormType> = async () => {
+    try {
+      setRequestStatus((draft) => {
+        draft.isLoading = true;
+      });
+      await mutateAsync(getValues);
+    } catch (error) {
+      onError();
     }
   };
 
