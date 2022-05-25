42e9cca39aa210292d3296d0f17753a1294bd116
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-05-25 06:33:04 +0000

    create button item container

--- src/pages/Account.page.tsx
+++ src/pages/Account.page.tsx
@@ -51,12 +51,20 @@ const Account: NextPageLayoutType = () => {
       isSuccess={isSuccess}
       isError={isError}
     >
-      <div className="rounded-lg bg-neutral-50 p-8">
-        <ItemContainer fieldValue="Name" value={userInfo.name} url="" />
-        <Divider />
-        <ItemContainer fieldValue="Email" value={userInfo.email} url="" />
-        <Divider />
-        <ItemContainer fieldValue="Password" value="*******" url="" />
+      <div className="max-w-2xl">
+        <div className="rounded-lg border-2 border-neutral-200 p-8">
+          <ItemContainer fieldValue="Name" value={userInfo.name} url="" />
+          <Divider />
+          <ItemContainer fieldValue="Email" value={userInfo.email} url="" />
+          <Divider />
+          <ItemContainer fieldValue="Password" value="*******" url="" />
+        </div>
+
+        <div className="mt-8 rounded-lg border-2 border-neutral-200 p-8">
+          <ItemContainerButton fieldName="Logout All" />
+          <Divider />
+          <ItemContainerButton fieldName="Delete Account" />
+        </div>
       </div>
     </SideBarContent>
   );
@@ -70,19 +78,29 @@ const ItemContainer: FC<{
 }> = ({ fieldValue, value, url, className }) => {
   return (
     <div className={classNames(className, "")}>
-      <p className="text-lg font-semibold">{fieldValue}</p>
+      <p className="text-base font-semibold">{fieldValue}</p>
       <div className="flex items-center justify-between">
-        <p className="">{value}</p>
+        <p className="text-sm">{value}</p>
         <Link href={url}>
-          <a className="rounded-md bg-indigo-600 px-5 py-2 text-white">EDIT</a>
+          <a className="rounded-sm bg-indigo-600 px-5 py-2 text-sm text-white">
+            EDIT
+          </a>
         </Link>
       </div>
     </div>
   );
 };
 
+const ItemContainerButton: FC<{ fieldName: string }> = ({ fieldName }) => {
+  return (
+    <button className="w-full text-center text-base font-semibold hover:text-indigo-700">
+      {fieldName}
+    </button>
+  );
+};
+
 const Divider: FC = () => (
-  <div className="my-4 h-0.5 rounded-full bg-neutral-200"></div>
+  <div className="my-5 h-0.5 rounded-full bg-neutral-200"></div>
 );
 
 Account.getLayout = AccountSideBarLayout;
---
+++ src/pages/Edit/Name.page.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Name: NextPage = () => {
+  return <>Name</>;
+};
+
+export default Name;
