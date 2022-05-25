25da59635f01804416e56270d155cebe77a608f8
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-05-25 05:10:12 +0000

    improve ui of account section

--- src/pages/Account.page.tsx
+++ src/pages/Account.page.tsx
@@ -5,6 +5,9 @@ import AccountSideBarLayout from "~/layout/AccountSideBarLayout";
 import GetUserQuery from "~/utils/gql/User/GetUser.gql";
 import useAuthRequestHook from "~/hooks/useAuthRequestHook";
 import SideBarContent from "~/components/Sidebar/SideBarContent";
+import { FC } from "react";
+import Link from "next/link";
+import { classNames } from "~/utils/helper/tailwind";
 
 const Account: NextPageLayoutType = () => {
   const [requestStatus, setRequestStatus] = useImmer({
@@ -48,11 +51,39 @@ const Account: NextPageLayoutType = () => {
       isSuccess={isSuccess}
       isError={isError}
     >
-      <h1>{userInfo.name}</h1>
-      <h1>{userInfo.email}</h1>
+      <div className="rounded-lg bg-neutral-50 p-8">
+        <ItemContainer fieldValue="Name" value={userInfo.name} url="" />
+        <Divider />
+        <ItemContainer fieldValue="Email" value={userInfo.email} url="" />
+        <Divider />
+        <ItemContainer fieldValue="Password" value="*******" url="" />
+      </div>
     </SideBarContent>
   );
 };
+
+const ItemContainer: FC<{
+  fieldValue: string;
+  value: string;
+  url: string;
+  className?: string;
+}> = ({ fieldValue, value, url, className }) => {
+  return (
+    <div className={classNames(className, "")}>
+      <p className="text-lg font-semibold">{fieldValue}</p>
+      <div className="flex items-center justify-between">
+        <p className="">{value}</p>
+        <Link href={url}>
+          <a className="rounded-md bg-indigo-600 px-5 py-2 text-white">EDIT</a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+const Divider: FC = () => (
+  <div className="my-4 h-0.5 rounded-full bg-neutral-200"></div>
+);
 
 Account.getLayout = AccountSideBarLayout;
 
