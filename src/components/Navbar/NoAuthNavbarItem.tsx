4548746671ef402f038139e7db5e91428b941326
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-05-29 10:46:08 +0000

    improve navbar ui

--- src/components/Navbar/NoAuthNavbarItem.tsx
+++ src/components/Navbar/NoAuthNavbarItem.tsx
@@ -3,16 +3,16 @@ import { ArrowSmRightIcon } from "@heroicons/react/outline";
 
 const NoAuthNavbarItem = () => {
   return (
-    <div className="flex">
+    <div className="text-medium flex text-sm">
       <Link href="/SignUp">
-        <a className="mx-4 rounded-md border-2 border-white px-4 py-1.5 hover:text-indigo-600">
-          SignUp
+        <a className="mx-4 rounded-md px-5 py-2.5 hover:text-indigo-700">
+          Login
         </a>
       </Link>
       <Link href="/Login">
-        <a className="flex items-center rounded-md border-2 border-slate-100 px-4 py-1.5 hover:text-indigo-600">
-          Login
-          <ArrowSmRightIcon className="h-4 pl-1" />
+        <a className="flex items-center rounded-md bg-indigo-600 px-5 py-2.5  text-white hover:bg-indigo-700">
+          Sign up
+          <ArrowSmRightIcon className="ml-1 h-5" />
         </a>
       </Link>
     </div>
