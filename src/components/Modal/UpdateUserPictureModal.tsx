c4a74b86c92f0e35b680c2f0467f7b55a1d1cddb
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-07-02 12:07:46 +0000

    use RemoveUserPictureOperation

--- src/components/Modal/UpdateUserPictureModal.tsx
+++ src/components/Modal/UpdateUserPictureModal.tsx
@@ -1,26 +1,69 @@
 import Image from "next/image";
+import { useQueryClient } from "react-query";
 import { Dispatch, FC, SetStateAction } from "react";
 
 import SmallButton from "~/components/Button/SmallButton";
 import ModalContainer from "~/components/Modal/Atom/ModalContainer";
 
+import { useAuthContext } from "~/context/AuthContext";
 import { useNotificationContext } from "~/context/NotificationContext";
+
+import useAuthMutationHook from "~/hooks/useAuthMutationHook";
+import RemoveUserPictureOperation from "~/utils/gql/User/RemoveUserPicture.gql";
+
+import {
+  RemoveUserPictureMutation,
+  RemoveUserPictureMutationVariables,
+} from "~/types/graphql";
 
 const UpdateUserPictureModal: FC<{
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
   picture: string;
 }> = ({ isOpen, setIsOpen, picture }) => {
-  const { addNotification } = useNotificationContext();
+  const queryClient = useQueryClient();
 
-  const isLoading = false;
+  const { setAuthFalse } = useAuthContext();
+  const { addNotification } = useNotificationContext();
 
   const errorNotification = () =>
     addNotification("error", "Something went wrong");
 
+  const { isLoading, mutate } = useAuthMutationHook<
+    RemoveUserPictureMutation,
+    RemoveUserPictureMutationVariables
+  >("RemoveUserPictureOperation", RemoveUserPictureOperation, () => ({}), {
+    onError: () => errorNotification(),
+    onSuccess: (data) => {
+      if (!data) return errorNotification();
+
+      const responseData = data.removeUserPicture;
+
+      switch (responseData.__typename) {
+        case "SuccessResponse":
+          queryClient.invalidateQueries("ReadUserFirstNameAndPictureOperation");
+          queryClient.invalidateQueries("ReadUserOperation");
+          exitModal();
+          break;
+
+        case "TokenError":
+          setAuthFalse();
+          break;
+
+        default:
+          errorNotification();
+          break;
+      }
+    },
+  });
+
   const exitModal = () => {
     if (isLoading) return;
     setIsOpen(false);
+  };
+
+  const handleRemovePicture = () => {
+    mutate();
   };
 
   return (
@@ -40,19 +83,23 @@ const UpdateUserPictureModal: FC<{
             />
           </div>
           <div className="ml-8">
-            <SmallButton
-              text="Remove"
-              type="submit"
-              className="border-red-600 bg-red-600 text-white hover:border-red-700 hover:bg-red-700 disabled:border-black disabled:bg-black"
-              isLoading={isLoading}
-            />
+            <fieldset disabled={isLoading}>
+              <SmallButton
+                text="Remove"
+                onClick={handleRemovePicture}
+                type="button"
+                className="border-red-600 bg-red-600 text-white hover:border-red-700 hover:bg-red-700 disabled:border-black disabled:bg-black"
+                isLoading={isLoading}
+              />
 
-            <SmallButton
-              text="Change"
-              type="submit"
-              className="mt-4 border-indigo-600 bg-indigo-600 text-white hover:border-indigo-700 hover:bg-indigo-700 disabled:border-black disabled:bg-black"
-              isLoading={isLoading}
-            />
+              <SmallButton
+                text="Change"
+                type="button"
+                className="mt-4 border-indigo-600 bg-indigo-600 text-white hover:border-indigo-700 hover:bg-indigo-700 disabled:border-black disabled:bg-black"
+                isLoading={isLoading}
+                onClick={exitModal}
+              />
+            </fieldset>
           </div>
         </div>
 
